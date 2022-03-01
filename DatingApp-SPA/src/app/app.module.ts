b1e56a418d52a3dfa34ba1caa1183813f8614714
Milos <milosvasic97@hotmail.com>
2022-03-01 15:18:21 +0100

    pagination of users

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDatepickerModule, BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDatepickerModule, BsDropdownModule, PaginationModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -60,6 +60,7 @@ export function tokenGetter() {
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
     FileUploadModule,
---
+++ Webb/Helpers/UserParams.cs
@@ -0,0 +1,16 @@
+namespace Webb.Helpers
+{
+    public class UserParams
+    {
+
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        
+    }
+}
