1250323697a7a9bb7dafb1ccbf0885765bf927be
Milos <milosvasic97@hotmail.com>
2022-03-03 14:13:27 +0100

    message controller part one

--- Webb/Data/DataContext.cs
+++ Webb/Data/DataContext.cs
@@ -15,6 +15,8 @@ namespace Webb.Data
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
 
+        public DbSet<Message> Messages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>()
@@ -30,6 +32,16 @@ namespace Webb.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessageSent)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+             builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessageReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
 
--- Webb/Data/IDatingRepository.cs
+++ Webb/Data/IDatingRepository.cs
@@ -13,8 +13,11 @@ namespace Webb.Data
         Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
-         Task<Photo> GetMainPhotoForUser(int userId);
-         Task<Like> GetLike(int userId, int recipientId);
+        Task<Photo> GetMainPhotoForUser(int userId);
+        Task<Like> GetLike(int userId, int recipientId);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
 
     }
 }
